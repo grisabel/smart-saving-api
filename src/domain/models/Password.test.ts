8ff17dd7481f6c77d70e60b3ec38885a63bc033a
IsabelGR <grisabel97@outlook.com>
2023-12-06 18:08:46 +0100

    feat/CA-1.3: add password requirements

--- src/domain/models/Password.test.ts
+++ src/domain/models/Password.test.ts
@@ -1,22 +1,58 @@
+import { Password, ERRORS } from './Password';
+describe('La clase Password', () => {
+  it('debe poder instanciarse mediante un string', () => {
+    //arrange
+    const password = 'Aabb@1';
+    //act
+    const newPassword = new Password(password);
+    //assert
+    expect(newPassword).toBeInstanceOf(Password);
+  });
+  it('debe lanzar un error si es menor a 6 caracteres', () => {
+    //arrange
+    const password = 'Aab@1';
 
-import {Password, ERRORS} from './Password'
-describe("La clase Password", () => {
-    it("debe poder instanciarse mediante un string", () => {
-        //arrange
-        const password = 'Aabb@1'
-        //act
-        const newPassword = new Password(password)
-        //assert
-        expect(newPassword).toBeInstanceOf(Password)
-    })
-    it("debe lanzar un error si es menor a 6 caracteres", () => {
-        //arrange
-        const password = 'Aab@1'
-    
-        //assert
-        expect(() => {
-            new Password(password)
-        }).toThrow(ERRORS.length)
-        
-    })
-})
+    //assert
+    expect(() => {
+      new Password(password);
+    }).toThrow(ERRORS.length);
+  });
+  it('debe lanzar un error si no tiene al menos un caracter numérico', () => {
+    //arrange
+    const password = 'Aaabb@';
+
+    //assert
+    expect(() => {
+      new Password(password);
+    }).toThrow(ERRORS.number);
+  });
+  it('debe lanzar un error si no tiene al menos una letra mayúscula', () => {
+    //arrange
+    const password = 'aaabb@1';
+
+    //assert
+    expect(() => {
+      new Password(password);
+    }).toThrow(ERRORS.upperCase);
+  });
+
+  it('debe lanzar un error si no tiene al menos una letra minúscula', () => {
+    //arrange
+    const password = 'AAABB@1';
+
+    //assert
+    expect(() => {
+      new Password(password);
+    }).toThrow(ERRORS.lowerCase);
+  });
+
+  it('debe lanzar un error si no tiene al menos un caracter especial de la siguiente lista: _ @ # !', () => {
+    //arrange
+    const password = 'AAAbB1';
+
+    //assert
+    expect(() => {
+      new Password(password);
+    }).toThrow(ERRORS.specialChar);
+  });
+});
--- src/domain/models/Password.ts
+++ src/domain/models/Password.ts
@@ -1,17 +1,49 @@
 export const ERRORS = {
-    length: "La contraseña debe tener al menos 6 caracteres."
-}
-const MINLENGTH = 6
+  length: 'La contraseña debe tener al menos 6 caracteres.',
+  number: 'La contraseña debe contener al menos un número.',
+  upperCase: 'La contraseña debe contener al menos una letra mayúscula.',
+  lowerCase: 'La contraseña debe contener al menos una letra minúscula.',
+  specialChar:
+    'La contraseña debe contener al menos alguno de los siguientes caracteres: _ @ # !',
+};
+const MINLENGTH = 6;
 
 export class Password {
-    private password:string
+  private password: string;
 
-    constructor(
-        password:string
-    ){
-        if(password.length < MINLENGTH){
-            throw new Error(ERRORS.length)
-        }
-        this.password = password
+  constructor(password: string) {
+    this.ensureMinLength(password);
+    this.ensureNumber(password);
+    this.ensureUpperCase(password);
+    this.ensureLowerCase(password);
+    this.ensureSpecialChar(password);
+
+    this.password = password;
+  }
+
+  ensureMinLength(password: string): void {
+    if (password.length < MINLENGTH) {
+      throw new Error(ERRORS.length);
     }
+  }
+  ensureNumber(password: string): void {
+    if (!/\d/.test(password)) {
+      throw new Error(ERRORS.number);
+    }
+  }
+  ensureUpperCase(password: string): void {
+    if (!/[A-Z]/.test(password)) {
+      throw new Error(ERRORS.upperCase);
+    }
+  }
+  ensureLowerCase(password: string): void {
+    if (!/[a-z]/.test(password)) {
+      throw new Error(ERRORS.lowerCase);
+    }
+  }
+  ensureSpecialChar(password: string): void {
+    if (!/[_@#!]/.test(password)) {
+      throw new Error(ERRORS.specialChar);
+    }
+  }
 }
