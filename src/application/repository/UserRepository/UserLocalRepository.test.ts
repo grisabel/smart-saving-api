d629b783a90295e8aff69ee78252f9e43427eec3
Delgado Trueba <jose.delgado@babel.es>
2023-12-07 17:41:20 +0100

    feat/CA-1.3: add beforeEach

--- src/application/repository/UserRepository/UserLocalRepository.test.ts
+++ src/application/repository/UserRepository/UserLocalRepository.test.ts
@@ -3,12 +3,15 @@ import { UserInterfaceRepository } from './UserInterfaceRepository';
 import { UserExample } from '@domain/models/User/test/User.example';
 
 describe('La clase UserLocalRepository', () => {
-  const user1 = UserExample.user1();
-  const user2 = UserExample.user2();
+  let userRepository: UserInterfaceRepository;
+
+  beforeEach(() => {
+    userRepository = new UserLocalRepository();
+  });
 
   it('guarda un usuario y comprueba si existe', () => {
     //arrange
-    const userRepository: UserInterfaceRepository = new UserLocalRepository();
+    const user1 = UserExample.user1();
     //act
     userRepository.save(user1);
     const promise = userRepository.findByEmail(user1.getEmail());
@@ -16,11 +19,13 @@ describe('La clase UserLocalRepository', () => {
     expect.assertions(1);
     expect(promise).resolves.toEqual(user1);
   });
+
   it('comprueba que no existe el usuario por email', () => {
     //arrange
-    const userRepository: UserInterfaceRepository = new UserLocalRepository();
+    const user1 = UserExample.user1();
+
     //act
-    const promise = userRepository.findByEmail(user2.getEmail());
+    const promise = userRepository.findByEmail(user1.getEmail());
     //assert
     expect.assertions(1);
     expect(promise).resolves.toEqual(null);
