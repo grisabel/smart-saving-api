1a9562e6eae873e377055f5594fe997594c20fb9
IsabelGR <grisabel97@outlook.com>
2023-12-08 12:12:22 +0100

    feat/CA-1.3: install cors

--- src/infrastructure/server.ts
+++ src/infrastructure/server.ts
@@ -1,6 +1,14 @@
 import express from 'express';
+import cors from 'cors';
 
 const server = express();
+
+server.use(
+  cors({
+    origin: '*',
+    methods: ['OPTIONS', 'GET', 'PUT', 'POST'],
+  })
+);
 
 server.get('/', (req, res) => {
   res.send({ message: 'Hello API' });
