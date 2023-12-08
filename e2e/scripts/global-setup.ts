f38deff24e4c72991b3b59d72cbd9ea1e084b787
Delgado Trueba <jose.delgado@babel.es>
2023-12-08 16:25:28 +0100

    fiX/CA-1.3: test e2e config

--- e2e/scripts/global-setup.ts
+++ e2e/scripts/global-setup.ts
@@ -1,3 +1,19 @@
+import app from '../../src/infrastructure/server';
+
+function startServer() {
+  const host = process.env.HOST ?? 'localhost';
+  const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+
+  return new Promise((resolve, reject) => {
+    const server = app.listen(port, host, () => {
+      console.log(`[ ready ] http://${host}:${port}`);
+      resolve(server);
+    });
+
+    server.on('error', reject);
+  });
+}
+
 /* eslint-disable */
 var __TEARDOWN_MESSAGE__: string;
 
@@ -5,6 +21,8 @@ module.exports = async function () {
   // Start services that that the app needs to run (e.g. database, docker-compose, etc.).
   console.log('\nSetting up...\n');
 
+  const server = await startServer();
   // Hint: Use `globalThis` to pass variables to global teardown.
+  globalThis.__server__ = server;
   globalThis.__TEARDOWN_MESSAGE__ = '\nTearing down...\n';
 };
