4a26ce2bd3a10d84087b9abe266dea56c82cff78
IsabelGR <grisabel97@outlook.com>
2023-12-11 21:10:12 +0100

    feat/CA-1.3: use jwt

--- src/domain/useCases/AuthenticateUseCase.ts
+++ src/domain/useCases/AuthenticateUseCase.ts
@@ -1,7 +1,10 @@
+import JWTService from '@application/services/JWTService';
 export class AuthenticateUseCase {
   authenticate(emailDTO: string, passwordDTO: string): Promise<string> {
     return new Promise((resolve, reject) => {
-      resolve('');
+      const user = { name: 'Name' };
+      const jwt = JWTService.createJWT(emailDTO, user);
+      resolve(jwt);
     });
   }
 }
