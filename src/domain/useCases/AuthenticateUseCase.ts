7d2ea0b3279bf249d160e14bc0428e8bbdf7d1f9
IsabelGR <grisabel97@outlook.com>
2023-12-11 21:55:59 +0100

    feat/CA-1.3: inject userRepository

--- src/domain/useCases/AuthenticateUseCase.ts
+++ src/domain/useCases/AuthenticateUseCase.ts
@@ -1,10 +1,35 @@
+import { UserInterfaceRepository } from '@application/repository/UserRepository/UserInterfaceRepository';
 import JWTService from '@application/services/JWTService';
+import { Email } from '@domain/models/Email';
+import { Password } from '@domain/models/Password';
+
 export class AuthenticateUseCase {
+  constructor(private userRepository: UserInterfaceRepository) {}
   authenticate(emailDTO: string, passwordDTO: string): Promise<string> {
-    return new Promise((resolve, reject) => {
-      const user = { name: 'Name' };
-      const jwt = JWTService.createJWT(emailDTO, user);
-      resolve(jwt);
+    return new Promise(async (resolve, reject) => {
+      try {
+        const email = Email.createFromText(emailDTO);
+        const passwordHash = Password.createFromHash(passwordDTO);
+        const user = await this.userRepository.findByEmail(email);
+
+        const match = passwordHash.isEqual(user.getPassword());
+
+        if (match) {
+          const userPayload = { name: user.getFirtname() };
+          const jwt = JWTService.createJWT(emailDTO, userPayload);
+          //todo crear dto
+          resolve(jwt);
+        } else {
+          //todo crear dto
+          console.log('reject');
+          reject();
+        }
+      } catch (error) {
+        console.log('error');
+        //todo añadir dto excepción email
+        //todo añadir dto excepción contraseña
+        //todo añadir dto excepcion usuario no encontrado
+      }
     });
   }
 }
