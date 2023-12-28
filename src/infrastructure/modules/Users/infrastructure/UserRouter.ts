bfcbe82178b116bb39bafbe8a1a908b1422504d6
IsabelGR <grisabel97@outlook.com>
2023-12-28 09:41:35 +0100

    feat/CA-5.7: move middlewares isLoggedIn

--- src/infrastructure/modules/Users/infrastructure/UserRouter.ts
+++ src/infrastructure/modules/Users/infrastructure/UserRouter.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import { validate } from '@infrastructure/middlewares/validators/validate';
-import { isLoggedIn } from '@infrastructure/middlewares/isLoggedIn/isLoggedIn';
 
+import { isLoggedIn } from '@Session/infrastructure/middlewares/isLoggedIn/isLoggedIn';
 import UserController from '@Users/infrastructure/controllers/UserController';
 import UserValidator from '@Users/infrastructure/validators/UserValidator';
 
