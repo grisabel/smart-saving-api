c132cdf58dc6bd7c281cc13889cd36a253cba7a6
Delgado Trueba <delgadotrueba@outlook.com>
2023-12-29 15:58:26 +0100

    feat/CA-3.8: add dotenv

--- src/infrastructure/config/index.ts
+++ src/infrastructure/config/index.ts
@@ -1,14 +1,19 @@
-export default {
+const config = {
   JWT: {
     ACCESS_TOKEN: {
       EXPIRES_TIME: '15m',
-      PRIVATE_KEY:
-        'Xena: la princesa guerrera cuenta las aventuras de la guerrera Xena',
+      PRIVATE_KEY: process.env.ACCESS_TOKEN_KEY,
     },
     REFRESH_TOKEN: {
       EXPIRES_TIME: '1d',
-      PRIVATE_KEY:
-        'Xena2: la princesa guerrera cuenta las aventuras de la guerrera Xena',
+      PRIVATE_KEY: process.env.REFRESH_TOKEN_KEY,
     },
   },
+  EMAIL: {
+    API_KEY: process.env.EMAIL_API_KEY,
+  },
 };
+
+console.log(config);
+
+export default config;
