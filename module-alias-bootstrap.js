269d9f513e7614d8d6753a7a96ccb814045cba3b
Delgado Trueba <delgadotrueba@outlook.com>
2024-01-02 22:30:17 +0000

    feat/CF-1.9: fix imports

--- module-alias-bootstrap.js
+++ module-alias-bootstrap.js
@@ -14,6 +14,10 @@ moduleAlias.addAliases({
     "@Users/infrastructure": outputPath+"/src/infrastructure/modules/Users/infrastructure",
     "@Users/application": outputPath+"/src/infrastructure/modules/Users/application",
     "@Users/domain": outputPath+"/src/infrastructure/modules/Users/domain",
+
+    "@FinancialControl/infrastructure": outputPath+"/src/infrastructure/modules/FinancialControl/infrastructure",
+    "@FinancialControl/application": outputPath+"/src/infrastructure/modules/FinancialControl/application",
+    "@FinancialControl/domain": outputPath+"/src/infrastructure/modules/FinancialControl/domain",
 })
 
 moduleAlias()
