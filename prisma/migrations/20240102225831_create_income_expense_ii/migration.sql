67e940adea0ede923c0dc8eb3253bfcf7018cca5
Delgado Trueba <delgadotrueba@outlook.com>
2024-01-02 22:58:46 +0000

    feat/CF-1.9: create table Income Expense II

--- prisma/migrations/20240102225831_create_income_expense_ii/migration.sql
+++ prisma/migrations/20240102225831_create_income_expense_ii/migration.sql
@@ -55,6 +55,35 @@ CREATE TABLE `RevokeAccessToken` (
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
+-- CreateTable
+CREATE TABLE `FinancialAccount` (
+    `accountId` INTEGER NOT NULL DEFAULT 0,
+    `userEmail` VARCHAR(191) NOT NULL,
+
+    INDEX `FinancialAccount_userEmail_accountId_idx`(`userEmail`, `accountId`),
+    PRIMARY KEY (`accountId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Income` (
+    `id` VARCHAR(191) NOT NULL,
+    `userEmail` VARCHAR(191) NOT NULL,
+    `amount` DOUBLE NOT NULL,
+    `accountNumber` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Expense` (
+    `id` VARCHAR(191) NOT NULL,
+    `userEmail` VARCHAR(191) NOT NULL,
+    `amount` DOUBLE NOT NULL,
+    `accountNumber` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 -- AddForeignKey
 ALTER TABLE `Password` ADD CONSTRAINT `Password_userEmail_fkey` FOREIGN KEY (`userEmail`) REFERENCES `User`(`email`) ON DELETE CASCADE ON UPDATE CASCADE;
 
@@ -63,3 +92,18 @@ ALTER TABLE `Operation` ADD CONSTRAINT `Operation_userEmail_fkey` FOREIGN KEY (`
 
 -- AddForeignKey
 ALTER TABLE `Session` ADD CONSTRAINT `Session_userEmail_fkey` FOREIGN KEY (`userEmail`) REFERENCES `User`(`email`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `FinancialAccount` ADD CONSTRAINT `FinancialAccount_userEmail_fkey` FOREIGN KEY (`userEmail`) REFERENCES `User`(`email`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Income` ADD CONSTRAINT `Income_userEmail_fkey` FOREIGN KEY (`userEmail`) REFERENCES `User`(`email`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Income` ADD CONSTRAINT `Income_accountNumber_fkey` FOREIGN KEY (`accountNumber`) REFERENCES `FinancialAccount`(`accountId`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Expense` ADD CONSTRAINT `Expense_userEmail_fkey` FOREIGN KEY (`userEmail`) REFERENCES `User`(`email`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Expense` ADD CONSTRAINT `Expense_accountNumber_fkey` FOREIGN KEY (`accountNumber`) REFERENCES `FinancialAccount`(`accountId`) ON DELETE CASCADE ON UPDATE CASCADE;
