0250559d24d9abf5a14dd32e687359f4ef0958c8
Delgado Trueba <delgadotrueba@outlook.com>
2024-01-13 22:29:59 +0000

    feat/CF-4.6: create get endpoint

--- src/infrastructure/modules/FinancialControl/domain/useCases/FinancialAccountUseCase.ts
+++ src/infrastructure/modules/FinancialControl/domain/useCases/FinancialAccountUseCase.ts
@@ -2,25 +2,32 @@ import { User } from '@domain/models/User';
 
 import { ErrorResponseMapper } from '@infrastructure/mappers/response/ErrorResponseMapper';
 import { ErrorResponseDto } from '@infrastructure/dtos/response/ErrorResponseDto';
-import { FinancialAccountInterfaceRepository, FinancialAccountRepositoryError } from '@infrastructure/modules/FinancialControl/application/repository/FinancialAccountRepository/FinancialAccountInterfaceRepository';
+import {
+  FinancialAccountInterfaceRepository,
+  FinancialAccountRepositoryError,
+} from '@infrastructure/modules/FinancialControl/application/repository/FinancialAccountRepository/FinancialAccountInterfaceRepository';
 import { FinancialAccountFactoryRepository } from '@FinancialControl/application/repository/FinancialAccountRepository/FinancialAccountFactoryRepository';
 import { Email } from '@domain/models/Email';
 import { FinancialAccountSummaryResponseDto } from '../../infrastructure/dtos/response/FinancialAccountSummaryResponseDto';
 
 export class FinancialAccountUseCase {
-  constructor(
-    private financialAccount: FinancialAccountInterfaceRepository
-  ) {}
+  constructor(private financialAccount: FinancialAccountInterfaceRepository) {}
 
-  obtainSummary(email: Email, accountNumber: number): Promise<[ErrorResponseDto | Error, FinancialAccountSummaryResponseDto]> {
+  obtainSummary(
+    email: Email,
+    accountNumber: number
+  ): Promise<[ErrorResponseDto | Error, FinancialAccountSummaryResponseDto]> {
     return new Promise(async (resolve, reject) => {
       try {
-        const result = await this.financialAccount.summary(email, accountNumber);
+        const result = await this.financialAccount.summary(
+          email,
+          accountNumber
+        );
 
         const resultDto: FinancialAccountSummaryResponseDto = {
-            expenses: result.expenses,
-            incomes: result.incomes
-        }
+          expenses: result.expenses,
+          incomes: result.incomes,
+        };
         resolve([null, resultDto]);
       } catch (error) {
         if (error instanceof FinancialAccountRepositoryError) {
@@ -45,7 +52,7 @@ export class FinancialAccountUseCaseFactory {
     if (!FinancialAccountUseCaseFactory.instance) {
       const financialAccountRepository =
         FinancialAccountFactoryRepository.getInstance();
-        
+
       FinancialAccountUseCaseFactory.instance = new FinancialAccountUseCase(
         financialAccountRepository
       );
