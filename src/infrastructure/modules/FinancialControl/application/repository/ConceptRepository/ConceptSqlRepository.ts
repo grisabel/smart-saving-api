aaaeeacac961e81b1d4702c8f89346c956700981
IsabelGR <grisabel97@outlook.com>
2024-01-15 18:05:39 +0000

    feat/CF.4.6: update concept

--- src/infrastructure/modules/FinancialControl/application/repository/ConceptRepository/ConceptSqlRepository.ts
+++ src/infrastructure/modules/FinancialControl/application/repository/ConceptRepository/ConceptSqlRepository.ts
@@ -7,44 +7,47 @@ import {
 import { Concept } from './models/Concept';
 import { ConceptType } from '@prisma/client';
 import { Email } from '@domain/models/Email';
+import { Id } from '@domain/models/Id/Id';
 
 export class ConceptSqlRepository implements ConceptInterfaceRepository {
   addInitialData(email: Email): Promise<void> {
     return new Promise((resolve, reject) => {
       const defaultIncomes = [
-        'Nómina',
-        'Rentas',
-        'Becas/Subvenciones',
-        'Apuestas/Juego',
+        { id: 'EXPENSE-1', concept: 'Nómina' },
+        { id: 'EXPENSE-2', concept: 'Rentas' },
+        { id: 'EXPENSE-3', concept: 'Becas/Subvenciones' },
+        { id: 'EXPENSE-4', concept: 'Apuestas/Juego' },
       ].map((income) => {
         return {
           userEmail: email.getValue(),
           type: ConceptType.Concept_Income,
-          concept: income,
+          concept: income.concept,
+          id: `${income.id}-${Id.createId().getValue()}`,
         };
       });
 
       const defaultExpenses = [
-        'Hipoteca/Alquiler/Comunidad',
-        'Alimentación',
-        'Mascotas',
-        'Combustible',
-        'Luz',
-        'Calefacción',
-        'Telefonía/Internet',
-        'Agua',
-        'Estudios',
-        'Ocio',
-        'Tasas/Impuestos/Multas',
-        'Educación',
-        'Salud',
-        'Seguros',
-        'Coche',
+        { id: 'INCOME-1', concept: 'Hipoteca/Alquiler/Comunidad' },
+        { id: 'INCOME-2', concept: 'Alimentación' },
+        { id: 'INCOME-3', concept: 'Mascotas' },
+        { id: 'INCOME-4', concept: 'Combustible' },
+        { id: 'INCOME-5', concept: 'Luz' },
+        { id: 'INCOME-6', concept: 'Calefacción' },
+        { id: 'INCOME-7', concept: 'Telefonía/Internet' },
+        { id: 'INCOME-8', concept: 'Agua' },
+        { id: 'INCOME-9', concept: 'Estudios' },
+        { id: 'INCOME-10', concept: 'Ocio' },
+        { id: 'INCOME-11', concept: 'Tasas/Impuestos/Multas' },
+        { id: 'INCOME-12', concept: 'Educación' },
+        { id: 'INCOME-13', concept: 'Salud' },
+        { id: 'INCOME-14', concept: 'Seguros' },
+        { id: 'INCOME-15', concept: 'Coche' },
       ].map((expense) => {
         return {
           userEmail: email.getValue(),
           type: ConceptType.Concept_Expense,
-          concept: expense,
+          concept: expense.concept,
+          id: `${expense.id}-${Id.createId().getValue()}`,
         };
       });
 
