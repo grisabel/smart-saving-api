252164a49980581341fdad09c3b73fa1b2198384
Delgado Trueba <delgadotrueba@outlook.com>
2024-01-20 00:51:51 +0000

    feat/INF-1.9: create repository

--- src/infrastructure/modules/FinancialControl/application/repository/FinancialAccountRepository/FinancialAccountSqlRepository.ts
+++ src/infrastructure/modules/FinancialControl/application/repository/FinancialAccountRepository/FinancialAccountSqlRepository.ts
@@ -9,6 +9,7 @@ import { Email } from '@domain/models/Email';
 import { DATE_FORMATS } from '@application/services/DateTimeService/constants';
 import DateTimeService from '@application/services/DateTimeService/DateTimeService';
 import { DateTimeModel } from '@application/services/DateTimeService/DateTimeInterfaceService';
+import { Transaction } from '../TransactionRepository/models/Transaction';
 
 export class FinancialAccountSqlRepository
   implements FinancialAccountInterfaceRepository
@@ -118,6 +119,126 @@ export class FinancialAccountSqlRepository
             };
           }),
         });
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
+  reportsIncome(
+    email: Email,
+    accountNumber: number,
+    dateTo: DateTimeModel,
+    dateFrom: DateTimeModel
+  ): Promise<Transaction[]> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const resulAccount = await prisma.financialAccount.findMany({
+          where: { userEmail: email.getValue(), accountNumber },
+        });
+
+        if (resulAccount.length === 0) {
+          const error = new FinancialAccountRepositoryError({
+            accountNotExist: FINANCIAL_ACCOUNT_REPOSITORY_ERROR.accountNotExist,
+          });
+          reject(error);
+        }
+
+        const dateStart = dateFrom;
+        const dateEnd = dateTo;
+
+        const resulIncome = await prisma.income.findMany({
+          where: {
+            AND: [
+              { accountId: resulAccount[0].id },
+              {
+                date: {
+                  gte: DateTimeService.parse(dateStart, DATE_FORMATS.ISO_8601),
+                },
+              },
+              {
+                date: {
+                  lte: DateTimeService.parse(dateEnd, DATE_FORMATS.ISO_8601),
+                },
+              },
+            ],
+          },
+        });
+
+        const incomes = resulIncome.map((income) => {
+          return {
+            amount: income.amount,
+            conceptId: income.conceptId,
+            date: DateTimeService.parse(
+              {
+                date: `${income.date.getTime()}`,
+                format: DATE_FORMATS.TimestampMs,
+              },
+              DATE_FORMATS.Date
+            ),
+            note: income.note,
+          };
+        });
+        resolve(incomes);
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
+  reportsExpense(
+    email: Email,
+    accountNumber: number,
+    dateTo: DateTimeModel,
+    dateFrom: DateTimeModel
+  ): Promise<Transaction[]> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const resulAccount = await prisma.financialAccount.findMany({
+          where: { userEmail: email.getValue(), accountNumber },
+        });
+
+        if (resulAccount.length === 0) {
+          const error = new FinancialAccountRepositoryError({
+            accountNotExist: FINANCIAL_ACCOUNT_REPOSITORY_ERROR.accountNotExist,
+          });
+          reject(error);
+        }
+
+        const dateStart = dateTo;
+        const dateEnd = dateFrom;
+
+        const resulExpense = await prisma.expense.findMany({
+          where: {
+            AND: [
+              { accountId: resulAccount[0].id },
+              {
+                date: {
+                  gte: DateTimeService.parse(dateStart, DATE_FORMATS.ISO_8601),
+                },
+              },
+              {
+                date: {
+                  lte: DateTimeService.parse(dateEnd, DATE_FORMATS.ISO_8601),
+                },
+              },
+            ],
+          },
+        });
+
+        const expenses = resulExpense.map((income) => {
+          return {
+            amount: income.amount,
+            conceptId: income.conceptId,
+            date: DateTimeService.parse(
+              {
+                date: `${income.date.getTime()}`,
+                format: DATE_FORMATS.TimestampMs,
+              },
+              DATE_FORMATS.Date
+            ),
+            note: income.note,
+          };
+        });
+        resolve(expenses);
       } catch (error) {
         reject(error);
       }
