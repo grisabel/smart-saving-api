04e56fc246ad2ff3b21f511febd629f6170d136c
Delgado Trueba <delgadotrueba@outlook.com>
2024-01-20 16:43:58 +0000

    feat/INF-5.5: fix initialData

--- src/infrastructure/modules/FinancialControl/application/repository/ConceptRepository/ConceptInterfaceRepository.ts
+++ src/infrastructure/modules/FinancialControl/application/repository/ConceptRepository/ConceptInterfaceRepository.ts
@@ -23,28 +23,28 @@ export class ConceptRepositoryError extends Error {
 
 export abstract class ConceptInterfaceRepository {
   static DEFAULT_INCOME = [
-    { id: 'EXPENSE-1', concept: 'Nómina' },
-    { id: 'EXPENSE-2', concept: 'Rentas' },
-    { id: 'EXPENSE-3', concept: 'Becas/Subvenciones' },
-    { id: 'EXPENSE-4', concept: 'Apuestas/Juego' },
+    { id: 'INCOME-1', concept: 'Nómina' },
+    { id: 'INCOME-2', concept: 'Rentas' },
+    { id: 'INCOME-3', concept: 'Becas/Subvenciones' },
+    { id: 'INCOME-4', concept: 'Apuestas/Juego' },
   ];
 
   static DEFAULT_EXPENSE = [
-    { id: 'INCOME-1', concept: 'Hipoteca/Alquiler/Comunidad' },
-    { id: 'INCOME-2', concept: 'Alimentación' },
-    { id: 'INCOME-3', concept: 'Mascotas' },
-    { id: 'INCOME-4', concept: 'Combustible' },
-    { id: 'INCOME-5', concept: 'Luz' },
-    { id: 'INCOME-6', concept: 'Calefacción' },
-    { id: 'INCOME-7', concept: 'Telefonía/Internet' },
-    { id: 'INCOME-8', concept: 'Agua' },
-    { id: 'INCOME-9', concept: 'Estudios' },
-    { id: 'INCOME-10', concept: 'Ocio' },
-    { id: 'INCOME-11', concept: 'Tasas/Impuestos/Multas' },
-    { id: 'INCOME-12', concept: 'Educación' },
-    { id: 'INCOME-13', concept: 'Salud' },
-    { id: 'INCOME-14', concept: 'Seguros' },
-    { id: 'INCOME-15', concept: 'Coche' },
+    { id: 'EXPENSE-1', concept: 'Hipoteca/Alquiler/Comunidad' },
+    { id: 'EXPENSE-2', concept: 'Alimentación' },
+    { id: 'EXPENSE-3', concept: 'Mascotas' },
+    { id: 'EXPENSE-4', concept: 'Combustible' },
+    { id: 'EXPENSE-5', concept: 'Luz' },
+    { id: 'EXPENSE-6', concept: 'Calefacción' },
+    { id: 'EXPENSE-7', concept: 'Telefonía/Internet' },
+    { id: 'EXPENSE-8', concept: 'Agua' },
+    { id: 'EXPENSE-9', concept: 'Estudios' },
+    { id: 'EXPENSE-10', concept: 'Ocio' },
+    { id: 'EXPENSE-11', concept: 'Tasas/Impuestos/Multas' },
+    { id: 'EXPENSE-12', concept: 'Educación' },
+    { id: 'EXPENSE-13', concept: 'Salud' },
+    { id: 'EXPENSE-14', concept: 'Seguros' },
+    { id: 'EXPENSE-15', concept: 'Coche' },
   ];
   abstract addInitialData(email: Email): Promise<void>;
   abstract addExpense(email: Email, concept: string): Promise<Concept>;
