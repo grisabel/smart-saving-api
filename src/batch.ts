9e3b5326c85441535c8dd49814b8df582c42f5c9
Delgado Trueba <delgadotrueba@outlook.com>
2024-01-21 22:02:56 +0000

    fix/CA-1.5: add cron task

--- src/batch.ts
+++ src/batch.ts
@@ -1,6 +1,8 @@
+import { AlimentationBatch } from '@infrastructure/modules/FinancialControl/infrastructure/batch/AlimentationBatch/AlimentationBatch';
 import cron from 'node-cron';
 
 cron.schedule('0 6 * * *', function () {
   console.log('Running a task every day at 6:00 AM');
-  // Your task code goes here
+
+  Promise.allSettled([AlimentationBatch()]);
 });
