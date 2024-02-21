c78231c44133c8700d914e134d429fed1ab59838
IsabelGR <grisabel97@outlook.com>
2024-02-21 20:45:17 +0000

    refactor/improve_ddbb: add connectionError

--- src/application/repository/db.ts
+++ src/application/repository/db.ts
@@ -1,3 +1,11 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client';
 
-export const prisma = new PrismaClient()
+export const prisma = new PrismaClient();
+
+export class DDBBConnectionError extends Error {
+  static msg: string = 'DDBBConnectionError';
+
+  constructor() {
+    super(DDBBConnectionError.msg);
+  }
+}
--- src/infrastructure/middlewares/validators/error/errorHandler.ts
+++ src/infrastructure/middlewares/validators/error/errorHandler.ts
@@ -1,13 +1,18 @@
+import { DDBBConnectionError } from '@application/repository/db';
 import { ErrorResponseDto } from '@infrastructure/dtos/response/ErrorResponseDto';
 import { NextFunction, Request, Response } from 'express';
 
 function errorHandler(
-  err: Error,
+  error: Error,
   req: Request,
   res: Response<ErrorResponseDto>,
   next: NextFunction
 ) {
-  console.log(err);
+  console.log(error);
+  if (error instanceof DDBBConnectionError) {
+    return res.status(500).json({ message: 'Server Internal Error' });
+  }
+
   return res.status(500).json({ message: 'Server Internal Error' });
 }
 
